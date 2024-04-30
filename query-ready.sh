bd2423ce58a36c4098269665fc9a724948c9fa43
J1vvoo <128969316+J1vvoo@users.noreply.github.com>
2024-04-30 14:33:06 +0900

    Add files via upload

---
+++ query-ready.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# query setting
+echo -e "\033[32m[+] Git clone\033[0m $@"
+read -p "Enter git clone address : " repository_url
+directory_name=$(basename "$repository_url")
+clone_directory_name="$directory_name"-repo
+
+mkdir -p /home/codevuln/target-repo/$directory_name/$clone_directory_name
+
+echo -e "\033[32m[+] git clone : /home/codevuln/target-repo/$directory_name/$clone_directory_name\033[0m $@"
+git clone --depth=1 "$repository_url" "/home/codevuln/target-repo/$directory_name/$clone_directory_name"
+
+mkdir "/home/codevuln/target-repo/$directory_name/sonarqube"
+
+python3 query-action.py $directory_name $clone_directory_name
