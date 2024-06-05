580ce92a78369996cba20883f724699df88a8b2b
J1vvoo <128969316+J1vvoo@users.noreply.github.com>
2024-06-05 17:34:20 +0900

    Create query-ready.py

---
+++ 0605/query-ready.py
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# query setting
+echo -e "\033[32m[+] Git clone\033[0m $@"
+read -p "Enter git clone address : " repository_url
+directory_name=$(basename "$repository_url")
+clone_directory_name="$directory_name"-repo
+
+mkdir -p /home/codevuln/target-repo/$directory_name/$clone_directory_name
+
+echo -e "\033[32m[+] git clone : /home/codevuln/target-repo/$directory_name/$clone_directory_name\033[0m $@"
+git clone --depth=1 "$repository_url" "/home/codevuln/target-repo/$directory_name/$clone_directory_name"
+
+mkdir "/home/codevuln/target-repo/$directory_name/sonarqube"
+
+python3 <<END
+from sonarqube import SonarQubeClient
+
+# SonarQube 서버 설정
+url = "http://localhost:9000"
+username = "admin"
+password = "admin"
+
+# SonarQube 클라이언트 초기화
+sonar = SonarQubeClient(sonarqube_url=url, username=username, password=password)
+END
+
+python3 query-action.py $directory_name $clone_directory_name
